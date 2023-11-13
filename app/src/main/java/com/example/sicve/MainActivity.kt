48a9c58bf0f74436b6ee958b824b2b00fcc7901d
salvatore catanzaro <salvatorecatanzaro@MacBook-Pro-di-salvatore-2.local>
2023-11-13 07:13:26 +0100

    Code refactoring main activity

--- app/src/main/java/com/example/sicve/MainActivity.kt
+++ app/src/main/java/com/example/sicve/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.sicve
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ComponentActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,17 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.login)
         getSupportActionBar()?.hide()
         // get reference to all views
-        var username = findViewById<EditText>(R.id.et_user_name)
-        var et_password = findViewById<EditText>(R.id.et_password)
-        var btn_reset = findViewById<Button>(R.id.btn_reset)
-        var btn_submit = findViewById<Button>(R.id.btn_submit)
+        var username = findViewById<EditText>(R.id.username)
+        var password = findViewById<EditText>(R.id.password)
+        var buttonReset = findViewById<Button>(R.id.button_reset)
+        var buttonSubmit = findViewById<Button>(R.id.button_submit)
 
-        btn_reset.setOnClickListener {
+        buttonReset.setOnClickListener {
             // clearing user_name and password edit text views on reset button click
             username.setText("")
-            et_password.setText("")
+            password.setText("")
         }
-        btn_submit.setOnClickListener {
+        buttonSubmit.setOnClickListener {
             // clearing user_name and password edit text views on reset button click
             val intent = Intent(this, Admin::class.java)
             intent.putExtra("username", username.getText().toString())
