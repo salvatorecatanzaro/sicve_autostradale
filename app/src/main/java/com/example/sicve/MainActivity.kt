3a1bde764b93b782422448df0d54a250f12d1246
salvatore catanzaro <salvatorecatanzaro@MacBook-Pro-di-salvatore-2.local>
2023-11-13 13:37:44 +0100

    basic login implemented

--- app/src/main/java/com/example/sicve/MainActivity.kt
+++ app/src/main/java/com/example/sicve/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.sicve
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sicve.login.HardCodedCredentialsAuthentication
+import com.example.sicve.login.UserAuthentication
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +30,18 @@ class MainActivity : AppCompatActivity() {
             // clearing user_name and password edit text views on reset button click
             val intent = Intent(this, AdminActivity::class.java)
             intent.putExtra("username", username.getText().toString())
-            startActivity(intent)
+            val auth : UserAuthentication = HardCodedCredentialsAuthentication(username=username.getText().toString(), password=password.getText().toString())
+            val result = auth.login()
+            val errorText = result.entries.iterator().next().key
+            if(result.contains("Success")) {
+                startActivity(intent)
+            }
+            else {
+                val errorTextview = findViewById<TextView>(R.id.error_message)
+                errorTextview.setText(errorText)
+                errorTextview.setVisibility(View.VISIBLE)
+
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/sicve/login/HardCodedCredentialsAuthentication.kt
@@ -0,0 +1,36 @@
+package com.example.sicve.login
+
+import com.example.sicve.entities.User
+
+class HardCodedCredentialsAuthentication(username : String, password : String) :  UserAuthentication(username, password) {
+    private var usr: String = username
+    private var pwd: String = password
+
+
+    override fun login(): Map<String, User> {
+        val lineList = mutableListOf<String>("admin admin admin admin ADMIN", "utente utente utente utente UTENTE")
+        val nonexistinguser = User("not not not not NOT".split(" "))
+        for (it: String in lineList) {
+            if (it.trim().startsWith("//")) {
+                continue  // Skipping comments
+            }
+            val dati = it.split(" ")
+            val user = User(dati)
+
+            if (usr == user.username) {
+                var message: String
+
+                if (pwd == user.password)
+                    message = "Success"
+                else
+                    message = "Password errata!"
+                return mapOf(message to user)
+            }
+
+        }
+
+        return mapOf("Utente non esiste" to nonexistinguser)
+
+
+    }
+}
