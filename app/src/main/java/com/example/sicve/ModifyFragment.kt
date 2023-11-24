a848dbc1583c0ae2c80becd79ba7893f862a4fb4
salvatore catanzaro <salvatorecatanzaro@MacBook-Pro-di-salvatore-2.local>
2023-11-24 20:05:17 +0100

    elements gets dynamically added to modify view

--- app/src/main/java/com/example/sicve/ModifyFragment.kt
+++ app/src/main/java/com/example/sicve/ModifyFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.Switch
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.sicve.entities.HighWay
+import com.example.sicve.entities.HighwayBlock
 import com.example.sicve.utils.DBHelper
 
 // TODO: Rename parameter arguments, choose names that match
@@ -41,70 +43,73 @@ class ModifyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_modify, container, false)
-        val constraintLayout = view.findViewById<ConstraintLayout>(R.id.constraint_layout_modify)
         val db : DBHelper = DBHelper(view.context)
         val dbr = db.readableDatabase
         var highway : HighWay? = HighWay.getHighway(dbr)
         var test = ""
+        var ystep = 0f
+        for(highWayBlock in highway?.highwayBlock.orEmpty())
+        {
+            generateTutorForm(highWayBlock, ystep, view)
+            ystep += 500f
+        }
 
+
+        return view
+    }
+
+    fun generateTutorForm(highWayBlock: HighwayBlock?, ystep: Float, view: View)
+    {
+        val constraintLayout = view.findViewById<ConstraintLayout>(R.id.constraint_layout_modify)
 
         val tutorAttivoSwitch : Switch = Switch(view.context)
         tutorAttivoSwitch.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
         tutorAttivoSwitch.x = 10f
-        tutorAttivoSwitch.y = 300f
+        tutorAttivoSwitch.y = 300f + ystep
         tutorAttivoSwitch.text = "Tutor attivo?"
         constraintLayout.addView(tutorAttivoSwitch)
 
         val nomeStazioneEntrataTextView : TextView = TextView(view.context)
         nomeStazioneEntrataTextView.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
         nomeStazioneEntrataTextView.x = 10f
-        nomeStazioneEntrataTextView.y = 400f
+        nomeStazioneEntrataTextView.y = 400f + ystep
         nomeStazioneEntrataTextView.text = "Stazione Entrata"
         constraintLayout.addView(nomeStazioneEntrataTextView)
 
         val nomeStazioneEntrataEditTextView : EditText = EditText(view.context)
         nomeStazioneEntrataEditTextView.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
         nomeStazioneEntrataEditTextView.x = 500f
-        nomeStazioneEntrataEditTextView.y = 360f
+        nomeStazioneEntrataEditTextView.y = 360f + ystep
         constraintLayout.addView(nomeStazioneEntrataEditTextView)
 
         val nomeStazioneUscitaTextView : TextView = TextView(view.context)
         nomeStazioneUscitaTextView.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
         nomeStazioneUscitaTextView.x = 10f
-        nomeStazioneUscitaTextView.y = 500f
+        nomeStazioneUscitaTextView.y = 500f + ystep
         nomeStazioneUscitaTextView.text = "Stazione Uscita"
         constraintLayout.addView(nomeStazioneUscitaTextView)
+
+        val nomeStazioneUscitaEditTextView : EditText = EditText(view.context)
+        nomeStazioneUscitaEditTextView.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        nomeStazioneUscitaEditTextView.x = 500f
+        nomeStazioneUscitaEditTextView.y = 460f + ystep
+        constraintLayout.addView(nomeStazioneUscitaEditTextView)
 
         val limiteVelocitaTextView : TextView = TextView(view.context)
         limiteVelocitaTextView.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
         limiteVelocitaTextView.x = 10f
-        limiteVelocitaTextView.y = 600f
+        limiteVelocitaTextView.y = 600f + ystep
         limiteVelocitaTextView.text = "Limite autovelox"
+
+        val limiteVelocitaEditTextView : EditText = EditText(view.context)
+        limiteVelocitaEditTextView.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        limiteVelocitaEditTextView.x = 500f
+        limiteVelocitaEditTextView.y = 560f + ystep
+        constraintLayout.addView(limiteVelocitaEditTextView)
+
         constraintLayout.addView(limiteVelocitaTextView)
-
-
-
-
-
-        /*val dynamicButton = Button(view.context)
-        // setting layout_width and layout_height using layout parameters
-        dynamicButton.setText("Button 4 added dynamically")
-        dynamicButton.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
-        dynamicButton.x = 10f
-        dynamicButton.y = 200f
-        constraintLayout.addView(dynamicButton)*/
-
-        /*val button = Button(this)
-        button.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
-        button.text = "Click me"
-        button.setOnClickListener(View.OnClickListener {
-            button.text = "You just clicked me"
-        })
-        button.setBackgroundColor(Color.GREEN)
-        button.setTextColor(Color.RED)
-        constraintLayout.addView(button);*/
-        return view
     }
+
 
     companion object {
         /**
